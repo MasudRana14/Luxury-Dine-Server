b95795f9341e24205249391a99cabb0aa3aaa1c5
MasudRana14 <iammasud34@gmail.com>
2023-11-18 21:00:11 +0600

    post foods data on mongoDB

--- index.js
+++ index.js
@@ -11,9 +11,6 @@ app.use(cors());
 app.use(express.json());
 
 
-
-
-
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ousunhm.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
